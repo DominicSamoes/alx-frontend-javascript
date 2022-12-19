183787e47193ebdc30697a0429d38acce9f1e639
DominicSamoes <dominicsamo@gmail.com>
2022-12-19 23:36:33 +0200

    0x00. ES6 Basics Task 3

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,8 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this..addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscooNeighborhoods.push(newNeighborhood);
-    return self.anFranciscoNeighborhoods;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
